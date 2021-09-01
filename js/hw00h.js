379e2d224773c7938f7eefb3bde846234475c05f
andrewras93 <andrewras93@gmail.com>
2021-09-01 13:27:20 +0200

    Added description of function.

--- js/hw00h.js
+++ js/hw00h.js
@@ -5,6 +5,11 @@ const go = function () {
     for(let i = 0; i < targets.length; ++i) {
         targets[i].innerHTML = 'Hello, World!';
     }
+
+    let description = document.getElementsByClassName('description');    
+    for(let i = 0; i < description.length; ++i) {
+        description[i].innerHTML = 'The following function loops through af set of HTML collections and sets their inner HTML values respectively.'
+    }
 }
 
 window.addEventListener('load', go);
